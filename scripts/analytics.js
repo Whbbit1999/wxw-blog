2b87245760b04f1251dcedb81d66f1214a1b97df
whbbit <luckyboy_dong@sina.com>
2022-12-31 16:20:38 +0800

    添加百度统计和google统计

---
+++ scripts/analytics.js
@@ -0,0 +1,22 @@
+"use strict";
+
+hexo.extend.injector.register("body_end", function () {
+  return `<script>
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?31181a38d70c8faf2c5bed0d93cab07b";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+  </script>
+
+  <script async="async" src="https://www.googletagmanager.com/gtag/js?id=G-FTR5YKFFCM"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+    gtag('config', 'G-FTR5YKFFCM');
+  </script>
+  `;
+});
