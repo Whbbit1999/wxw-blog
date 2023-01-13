7d0cf18d024cf05f6e1ae26517c67c58c121fcf7
whbbit <luckyboy_dong@sina.com>
2023-01-14 00:03:00 +0800

    feat: 网页新增底部导航栏

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,6 +1,6 @@
-"use strict";
+// "use strict";
 
-// google ad
-hexo.extend.injector.register("head_end", function () {
-  return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7949385915241566" crossorigin="anonymous"></script>`;
-});
+// // google ad
+// hexo.extend.injector.register("head_end", function () {
+//   return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7949385915241566" crossorigin="anonymous"></script>`;
+// });
