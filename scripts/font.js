d498f496a2a5e17aaa75e2fdf056bdd81f070cd2
whbbit <luckyboy_dong@sina.com>
2023-02-04 09:30:47 +0800

    feat: 添加自定义友链功能，博客正文字体改动

---
+++ scripts/font.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// inject font
+hexo.extend.injector.register("head_end", () => {
+  return `
+    <style>
+      @font-face {
+        font-family: 'hytmr';
+        font-style: normal;
+        font-weight: normal;
+        font-display: swap;
+        src: url(https://cdn.coolchong.cn/%E5%AD%97%E4%BD%93%E6%A0%B7%E5%BC%8F/hytmr55.woff) format('woff');
+      }    
+    </style>
+  `;
+});
