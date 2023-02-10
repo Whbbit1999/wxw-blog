172dc0266db6f4a7c0b349bc32d1e9f1b3657cad
whbbit <luckyboy_dong@sina.com>
2023-02-10 12:31:02 +0800

    feat: 复制添加弹窗

---
+++ scripts/bilibiliIframe.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// bilibili iframe style
+hexo.extend.injector.register("head_end", function () {
+  return `
+    <style>
+      .bilibili-video {
+        width: 100%;
+        height: 500px
+      }
+    </style>
+
+
+    `;
+});
+// <iframe src="//player.bilibili.com/player.html?bvid=BV1VY411Q7FS" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" class="bilibili-video"> </iframe>
--- scripts/copyCode.js
+++ scripts/copyCode.js
@@ -1,5 +1,11 @@
 "use strict";
 
+hexo.extend.injector.register("body_end", () => {
+  return `
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/izitoast@1.4.0/dist/css/iziToast.min.css">
+  `;
+});
+
 // code block copy
 hexo.extend.injector.register("body_end", function () {
   return `
@@ -27,8 +33,13 @@ hexo.extend.injector.register("body_end", function () {
       opacity: 0.75;
 
     }
+    .highlight .code .copy-btn.warning {
+      color: var(--text-code);
+      opacity: 0.75;
+    }
     
   </style>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/izitoast@1.4.0/dist/js/iziToast.min.js"></script>
   <script>
     const codeElementArr = document.querySelectorAll('.code')
     codeElementArr.forEach(code => {
@@ -58,17 +69,76 @@ hexo.extend.injector.register("body_end", function () {
     })
     
     async function copyCode(currentCode) {
-      console.log(currentCode)
-      console.log('复制代码')
+      // console.log(currentCode)
+      // console.log('复制代码')
       if (navigator.clipboard) {
         try {
           await navigator.clipboard.writeText(currentCode)
+
+          iziToast.info({
+            timeout: 3000, // 关闭弹窗的时间
+            // icon: 'Fontawesome', // 图标类别
+            closeOnEscape: 'true', // 允许使用Esc键关闭弹窗
+            transitionIn: 'bounceInLeft', // 弹窗打开动画
+            transitionOut: 'fadeOutRight', // 弹窗关闭动画
+            displayMode: 'replace', // 替换已经打开的弹窗
+            layout: '2', // Medium模式
+            position: 'topRight', // 弹窗位置
+            //icon: 'fad fa-copy', // 图标类名
+            iconUrl:'https://image-1309791158.cos.ap-guangzhou.myqcloud.com/其他/1122star.svg',
+            backgroundColor: '#03cafc', // 弹窗背景色
+            title: '耶，复制成功了', // 通知标题
+            message: '＼(☆o☆)／，希望能对你有点帮助吧' // 通知消息内容
+          });
         } catch (error) {
           // 未获得用户许可
-          console.error(error)
+          codeCopyBtn.innerText = '未获得用户许可'
+          codeCopyBtn.classList.add('warning')
+          setTimeout(() => {
+            codeCopyBtn.innerText = '复制代码'
+            codeCopyBtn.classList.remove('warning')
+          },1000)
+
+          iziToast.info({
+            timeout: 3000, // 关闭弹窗的时间
+            // icon: 'Fontawesome', // 图标类别
+            closeOnEscape: 'true', // 允许使用Esc键关闭弹窗
+            transitionIn: 'bounceInLeft', // 弹窗打开动画
+            transitionOut: 'fadeOutRight', // 弹窗关闭动画
+            displayMode: 'replace', // 替换已经打开的弹窗
+            layout: '2', // Medium模式
+            position: 'topRight', // 弹窗位置
+            //icon: 'fad fa-copy', // 图标类名
+            iconUrl:'https://image-1309791158.cos.ap-guangzhou.myqcloud.com/其他/1122star.svg',
+            backgroundColor: '#f53900', // 弹窗背景色
+            color: '#fff',
+            title: '没有获得您的许可，复制失败了', // 通知标题
+            // message: '我的原则，只有三个字，看心情' // 通知消息内容
+          });
         }
       } else {
-        console.error('当前浏览器不支持此api')
+        codeCopyBtn.innerText = '当前浏览器不支持此api'
+        codeCopyBtn.classList.add('warning')
+        setTimeout(() => {
+          codeCopyBtn.innerText = '复制代码'
+          codeCopyBtn.classList.remove('warning')
+        },1000)
+
+        iziToast.info({
+          timeout: 3000, // 关闭弹窗的时间
+          // icon: 'Fontawesome', // 图标类别
+          closeOnEscape: 'true', // 允许使用Esc键关闭弹窗
+          transitionIn: 'bounceInLeft', // 弹窗打开动画
+          transitionOut: 'fadeOutRight', // 弹窗关闭动画
+          displayMode: 'replace', // 替换已经打开的弹窗
+          layout: '2', // Medium模式
+          position: 'topRight', // 弹窗位置
+          //icon: 'fad fa-copy', // 图标类名
+          iconUrl:'https://image-1309791158.cos.ap-guangzhou.myqcloud.com/其他/1122star.svg',
+          backgroundColor: '#f53900', // 弹窗背景色
+          title: '当前浏览器不支持此api', // 通知标题
+          message: '听我的，换个浏览器吧' // 通知消息内容
+        });
       }
     }
   </script>
