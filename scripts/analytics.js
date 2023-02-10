4bf1ae2e5ffce857d3bcb696619d5c085f59d379
whbbit <luckyboy_dong@sina.com>
2023-02-10 12:43:45 +0800

    feat: 去除百度统计，代码高亮更改为默认选项

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -2,15 +2,16 @@
 
 // 百度统计和google 统计
 hexo.extend.injector.register("body_end", function () {
-  return `<script>
-  var _hmt = _hmt || [];
-  (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?31181a38d70c8faf2c5bed0d93cab07b";
-    var s = document.getElementsByTagName("script")[0]; 
-    s.parentNode.insertBefore(hm, s);
-  })();
-  </script>
+  return `
+  // <script>
+  // var _hmt = _hmt || [];
+  // (function() {
+  //   var hm = document.createElement("script");
+  //   hm.src = "https://hm.baidu.com/hm.js?31181a38d70c8faf2c5bed0d93cab07b";
+  //   var s = document.getElementsByTagName("script")[0]; 
+  //   s.parentNode.insertBefore(hm, s);
+  // })();
+  // </script>
 
   <script async="async" src="https://www.googletagmanager.com/gtag/js?id=G-FTR5YKFFCM"></script>
   <script>
