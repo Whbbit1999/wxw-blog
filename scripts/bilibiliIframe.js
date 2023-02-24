3e31fa55a2ac92d6fe59ff136aac2e717ed8f02e
whbbit <luckyboy_dong@sina.com>
2023-02-24 23:27:42 +0800

    fit: 网站底部出现注释修复

--- scripts/bilibiliIframe.js
+++ scripts/bilibiliIframe.js
@@ -1,5 +1,6 @@
 // "use strict";
 
+// <iframe src="//player.bilibili.com/player.html?bvid=BV1VY411Q7FS" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" class="bilibili-video"> </iframe>
 // bilibili iframe style
 hexo.extend.injector.register("head_end", function () {
   return `
@@ -13,4 +14,3 @@ hexo.extend.injector.register("head_end", function () {
 
     `;
 });
-// <iframe src="//player.bilibili.com/player.html?bvid=BV1VY411Q7FS" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" class="bilibili-video"> </iframe>
